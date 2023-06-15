9361eb00d1ea360512854182672263b1fe04196d
Akash-879 <131871283+Akash-879@users.noreply.github.com>
2023-06-15 15:12:10 +0530

    Delete Ak directory

--- Ak/index.js
+++
@@ -1,9 +0,0 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").text(profile.getImageUrl());
-    $(".data").css("display","block");
-    $(".g-siginin2").css("display","none");
-
-  }
